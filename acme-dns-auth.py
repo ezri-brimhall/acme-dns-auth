aed0da14930452bec8ab523220d8446811c61fc1
Ezri Brimhall <ezri.brimhall@usu.edu>
2024-09-05 11:22:21 -0600

    Improved propagation delay logic and added support for reading from a config file

--- acme-dns-auth.py
+++ acme-dns-auth.py
@@ -3,8 +3,23 @@ import json
 import os
 import requests
 import sys
+import dns.resolver
+import time
+import configparser
 
-# EDIT THESE: Configuration values #
+config = configparser.ConfigParser()
+
+# Time to wait for DNS propagation before checking the TXT record
+DNS_PROPAGATION_PRE_WAIT = 30
+
+# Time to wait for DNS propagation between checks
+DNS_PROPAGATION_WAIT = 10
+
+# Maximum time to wait for DNS propagation
+DNS_PROPAGATION_TIMEOUT = 300
+
+# Extra wait time for DNS propagation, for safety
+DNS_PROPAGATION_POST_WAIT = 10
 
 # URL to acme-dns instance
 ACMEDNS_URL = "https://acmedns.usu.edu"
@@ -16,6 +31,26 @@ ALLOW_FROM = ["129.123.0.0/16", "144.39.0.0/16"]
 # Force re-registration. Overwrites the already existing acme-dns accounts.
 FORCE_REGISTER = False
 
+if os.path.exists("/etc/letsencrypt/acmedns.ini"):
+    config.read("/etc/letsencrypt/acmedns.ini")
+    if "openipam" in config:
+        OPENIPAM_TOKEN = config["openipam"].get("token")
+    if "acme" in config:
+        ACMEDNS_URL = config["acmedns"].get("url", "https://acmedns.usu.edu")
+        STORAGE_PATH = config["acmedns"].get("storage", "/etc/letsencrypt/acmedns.json")
+        FORCE_REGISTER = config["acmedns"].getboolean("force_register", fallback=False)
+    if "dns" in config:
+        NAMESERVERS = (
+            config["dns"].get("nameservers", "129.123.0.1 129.123.0.2").split()
+        )
+        DNS_PROPAGATION_PRE_WAIT = config["dns"].getint("propagation_pre_wait", 30)
+        DNS_PROPAGATION_WAIT = config["dns"].getint("propagation_wait", 10)
+        DNS_PROPAGATION_TIMEOUT = config["dns"].getint("propagation_timeout", 300)
+        DNS_PROPAGATION_POST_WAIT = config["dns"].getint("propagation_post_wait", 10)
+else:
+    pass
+
+
 #   DO NOT EDIT BELOW THIS POINT   #
 #         HERE BE DRAGONS          #
 
@@ -24,6 +59,11 @@ if DOMAIN.startswith("*."):
     DOMAIN = DOMAIN[2:]
 VALIDATION_DOMAIN = "_acme-challenge." + DOMAIN
 VALIDATION_TOKEN = os.environ["CERTBOT_VALIDATION"]
+
+try:
+    dns.resolver.get_default_resolver().nameservers = NAMESERVERS
+except NameError:
+    pass
 
 
 class AcmeDnsClient(object):
@@ -139,6 +179,124 @@ class Storage(object):
             return None
 
 
+def create_cname(openipam_url: str, auth_token: str, domain: str, content: str) -> bool:
+    """Create a CNAME record with OpenIPAM."""
+
+    try:
+        res = requests.post(
+            f"{openipam_url}/api/dns/add/",
+            data={
+                "name": domain,
+                "dns_type": "CNAME",
+                "content": content,
+                # TTL should be less than the propagation timeout
+                "ttl": 60,
+            },
+            headers={"Authorization": f"Token {auth_token}"},
+        )
+        res.raise_for_status()
+        return True
+    except requests.exceptions.RequestException as e:
+        print(f"Failed to create CNAME record: {e}")
+        if res.status_code == 400:
+            print(f"Response: {res.json()}")
+        return False
+
+
+def delete_cname(openipam_url: str, auth_token: str, domain: str) -> bool:
+    """Delete a CNAME record with OpenIPAM."""
+
+    try:
+        res = requests.get(
+            f"{openipam_url}/api/dns/?name={domain}&type=CNAME&limit=0",
+            headers={"Authorization": f"Token {auth_token}"},
+        )
+        res.raise_for_status()
+        # When limit is 0, the API does not construct a paginated response, and just returns an array
+        # of records. We can safely assume that the response is an array.
+        records = [record["id"] for record in res.json()]
+        if len(records) == 0:
+            print(f"No CNAME record found for {domain}")
+            return True
+        # There really only should be one record, but we'll loop through all of them just in case
+        for record in records:
+            res = requests.delete(
+                f"{openipam_url}/api/dns/{record}/delete",
+                headers={"Authorization": f"Token {auth_token}"},
+            )
+            res.raise_for_status()
+        return True
+    except requests.exceptions.RequestException as e:
+        print(f"Failed to delete DNS record: {e}")
+        return False
+
+
+def validate_record(domain: str, expected: str) -> bool:
+    answers = dns.resolver.resolve(VALIDATION_DOMAIN, "TXT")
+    for answer in answers:
+        if answer.to_text() == f'"{VALIDATION_TOKEN}"':
+            return True
+    return False
+
+
+def do_add(client, storage, account, openipam_url):
+
+    try:
+        auth_token = OPENIPAM_TOKEN
+    except NameError:
+        auth_token = os.environ.get("OPENIPAM_TOKEN")
+        print(auth_token)
+    if auth_token:
+        auto_added = create_cname(
+            openipam_url, auth_token, VALIDATION_DOMAIN, account["fulldomain"]
+        )
+        if auto_added:
+            storage.put(DOMAIN, account)
+            storage.save()
+            # Update the TXT record in acme-dns instance
+            client.update_txt_record(account, VALIDATION_TOKEN)
+            try:
+                wait_for_propagation(VALIDATION_DOMAIN, VALIDATION_TOKEN)
+            except:
+                print("DNS propagation failed.")
+                return False
+            return True
+        else:
+            print(f"Failed to create CNAME record for {VALIDATION_DOMAIN}")
+            print("It may already exist. If so, please delete it and try again.")
+            print(
+                "To add the record manually, remove the OPENIPAM_TOKEN configuration / environment variable and run this script again"
+            )
+            return False
+    else:
+        # Display the notification for the user to update the main zone
+        msg = "Please add the following CNAME record to your main DNS zone:\n{}"
+        cname = "{} CNAME {}.".format(VALIDATION_DOMAIN, account["fulldomain"])
+        print(msg.format(cname))
+        # Update the TXT record in acme-dns instance
+        client.update_txt_record(account, VALIDATION_TOKEN)
+        return False
+
+
+def wait_for_propagation(domain, secret):
+    """Wait for the DNS record to propagate."""
+    print("Waiting for DNS records to propagate...")
+    time.sleep(DNS_PROPAGATION_PRE_WAIT)
+    start = time.time()
+    propagated = False
+    while not propagated:
+        try:
+            if validate_record(domain, secret):
+                propagated = True
+        except dns.resolver.NXDOMAIN:
+            pass
+        time.sleep(DNS_PROPAGATION_WAIT)
+        if time.time() - start > DNS_PROPAGATION_TIMEOUT:
+            raise Exception("Timeout waiting for DNS records to propagate.")
+
+    time.sleep(DNS_PROPAGATION_POST_WAIT)
+
+
 if __name__ == "__main__":
     # Init
     client = AcmeDnsClient(ACMEDNS_URL)
@@ -147,52 +305,31 @@ if __name__ == "__main__":
     # Check if an account already exists in storage
     account = storage.fetch(DOMAIN)
 
+    openipam_url = os.environ.get("OPENIPAM_URL", "https://openipam.usu.edu")
+
     auto_added = False
     if FORCE_REGISTER or not account:
-        # Create and save the new account
         account = client.register_account(ALLOW_FROM)
-        storage.put(DOMAIN, account)
-        storage.save()
 
-        openipam_url = os.environ.get("OPENIPAM_URL", "https://openipam.usu.edu")
+        if not do_add(client, storage, account, openipam_url):
+            sys.exit(1)
+    else:
+        # Verify that the record is correct by querying DNS
         try:
-            auth_token = OPENIPAM_TOKEN
-        except NameError:
-            auth_token = os.environ.get("OPENIPAM_TOKEN")
-        if auth_token:
-            try:
-                res = requests.post(
-                    f"{openipam_url}/api/dns/add/",
-                    data={
-                        "name": VALIDATION_DOMAIN,
-                        "dns_type": "CNAME",
-                        "content": account["fulldomain"],
-                    },
-                    headers={
-                        "Authorization": f"Token {auth_token}",
-                    },
+            valid = validate_record(VALIDATION_DOMAIN, VALIDATION_TOKEN)
+        except dns.resolver.NXDOMAIN:
+            if not do_add(client, storage, account, openipam_url):
+                sys.exit(1)
+
+        if not valid:
+            print("Existing record is incorrect. Updating...")
+            if delete_cname(openipam_url, OPENIPAM_TOKEN, VALIDATION_DOMAIN):
+                if not do_add(client, storage, account, openipam_url):
+                    sys.exit(1)
+            else:
+                print("Failed to update CNAME record.")
+                print(
+                    "Please update the record to the following and run this script again:"
                 )
-                if res.status_code != 201:
-                    raise Exception(f"Failed to add DNS record: {res.text}")
-                auto_added = True
-            except Exception as e:
-                print(f"Error: {e}")
-                print("Unable to create DNS record automatically.")
-                msg = "Please add the following CNAME record to your main DNS zone:\n{}"
-                cname = "{} CNAME {}.".format(VALIDATION_DOMAIN, account["fulldomain"])
-                print(msg.format(cname))
-        else:
-            # Display the notification for the user to update the main zone
-            msg = "Please add the following CNAME record to your main DNS zone:\n{}"
-            cname = "{} CNAME {}.".format(VALIDATION_DOMAIN, account["fulldomain"])
-            print(msg.format(cname))
-
-    # Update the TXT record in acme-dns instance
-    client.update_txt_record(account, VALIDATION_TOKEN)
-
-    # Wait for the DNS record to propagate
-    if auto_added:
-        import time
-
-        print("Waiting for DNS records to propagate...")
-        time.sleep(10)
+                print(f"{VALIDATION_DOMAIN} CNAME {account['fulldomain']}.")
+                sys.exit(1)
